ca60b1e12c61239766f025e44f07639d1cbc7f06
carvalen <carmenpilar1990@gmail.com>
2022-05-22 19:17:43 +0200

    Add minBalance

--- banking-system/src/main/java/com/ironhack/bankingsystem/controllers/impl/ThirdPartyController.java
+++ banking-system/src/main/java/com/ironhack/bankingsystem/controllers/impl/ThirdPartyController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/third_party")
+@RequestMapping("/api/thirdparty")
 public class ThirdPartyController implements ThirdPartyControllerInterface {
 
 
